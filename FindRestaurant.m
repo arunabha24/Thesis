9116710691e2dad1a9aa13c31c93ca1d310b4b86
Arunabha Choudhury <arunabha_choudhury24@yahoo.com>
2014-05-09 13:18:39 -0500

    commit home2

---
+++ FindRestaurant.m
@@ -0,0 +1,21 @@
+function tempRestData = FindRestaurant(restData,rating)
+
+%FINDRESTAURANT' Summary of this function goes here
+
+%  Detailed explanation goes here
+
+
+
+%find the unique restaurants in rating data
+
+uRestRate = unique(rating(:,1));
+
+
+
+
+
+
+
+
+
+end
